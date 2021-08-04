1ce9dc661ddae19f4315c8fe7512b7dcd208d839
Medveddys <41831529+Medveddys@users.noreply.github.com>
2021-08-04 17:12:26 +0300

    Repaired login design, added true FP link

--- Task 1-2/CifroVuzV4/app/src/main/java/com/example/cifrovuzv4/MainActivity.kt
+++ Task 1-2/CifroVuzV4/app/src/main/java/com/example/cifrovuzv4/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.cifrovuzv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.method.LinkMovementMethod.*
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupHyperlink();
+    }
+    fun setupHyperlink() {
+        val linkTextView = findViewById<TextView>(R.id.textView4)
+        linkTextView.setMovementMethod(getInstance());
     }
 }
