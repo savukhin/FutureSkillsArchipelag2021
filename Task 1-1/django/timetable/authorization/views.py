a445be4fafbbab17147d520d89c02ce1f2993f97
Савелий <savukhin@gmail.com>
2021-08-04 13:45:20 +0300

    Added password changing

--- Task 1-1/django/timetable/authorization/views.py
+++ Task 1-1/django/timetable/authorization/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import CustomUser, CustomAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.utils.encoding import force_text, force_bytes
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from .tokens import account_activation_token
+from django.views import View
+from django.http import HttpResponseNotFound, HttpResponse
 
 # Create your views here.
 
@@ -35,6 +41,38 @@ def loginView(request):
     return render(request, template_name='login.html')
 
 
+def logoutView(request):
+    logout(request)
+    return redirect('/')
+
+
+class NewPassword(View):
+    def get_user(self, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        return user
+
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64, token)
+
+        if user is not None and account_activation_token.check_token(user, token):
+            return render(request, 'newPassword.html')
+        else:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
+
+    def post(self, request, uidb64, token):
+        user = self.get_user(uidb64, token)
+        if user is not None and account_activation_token.check_token(user, token):
+            user.set_password(request.POST['password1'])
+            user.save()
+            return redirect('/')
+        else:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
+
+
 def forgot_password(request):
     if request.method == 'POST':
         users = User.objects.filter(username=request.POST['username'], email=request.POST['email'])
@@ -43,6 +81,9 @@ def forgot_password(request):
         elif len(users) > 1:
             return render(request, template_name='forgotPassword.html', context={'errors': 'User more than one!'})
         else:
-            return redirect('/')
+            token = account_activation_token.make_token(users[0])
+            print(token)
+            print(users[0].id)
+            return redirect('/newPassword/' + urlsafe_base64_encode(force_bytes(users[0].id)) + "/" + token)
 
     return render(request, template_name='forgotPassword.html')
