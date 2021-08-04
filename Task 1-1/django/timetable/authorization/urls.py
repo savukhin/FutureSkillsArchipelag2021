273ce82a8ee0e8cfe5f60267694a938235c898f9
Савелий <savukhin@gmail.com>
2021-08-04 11:22:02 +0300

    Added login

--- Task 1-1/django/timetable/authorization/urls.py
+++ Task 1-1/django/timetable/authorization/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
+    path('login', views.loginView, name='login'),
+
+    path('forgotPassword', views.forgot_password, name='forgotPassword'),
+    # url(r'^password/change/$', views.PasswordChangeView.as_view(), name='password_change'),
+    # url(r'^password/change/done/$', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # url(r'^password/reset/$', views.PasswordResetView.as_view(), name='password_reset'),
+    # url(r'^password/reset/done/$', views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
+    # url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #
+    # url(r'^password/reset/complete/$', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+# </token></uidb64>
--- Task 1-1/django/timetable/authorization/views.py
+++ Task 1-1/django/timetable/authorization/views.py
@@ -2,17 +2,15 @@ from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib.auth import authenticate, login
 from .models import CustomUser, CustomAdmin
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def register(request):
-    print(request.method)
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        print("OK")
         if form.is_valid():
-            print("VALID")
             form.save()
             newCustomUser = CustomUser(user=form.instance)
             newCustomUser.save()
@@ -21,4 +19,30 @@ def register(request):
             return redirect('/')
         return render(request, template_name='register.html', context={'form': form})
 
-    return render(request, template_name='register.html')
+    return render(request, template_name='registration.html')
+
+
+def loginView(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if not user:
+            return render(request, template_name='login.html',
+                          context={"errors": "User or password is not correct!"})
+
+        login(request, user)
+        return redirect('/')
+
+    return render(request, template_name='login.html')
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        users = User.objects.filter(username=request.POST['username'], email=request.POST['email'])
+        if len(users) == 0:
+            return render(request, template_name='forgotPassword.html', context={'errors': 'User does not exists!'})
+        elif len(users) > 1:
+            return render(request, template_name='forgotPassword.html', context={'errors': 'User more than one!'})
+        else:
+            return redirect('/')
+
+    return render(request, template_name='forgotPassword.html')
