e5322e98e0566aea97c01ad3db3f1ab0960e45ce
Савелий <savukhin@gmail.com>
2021-08-05 10:53:28 +0300

    fixed migrations

--- Task 1-1/django/timetable/lessons/forms.py
+++ Task 1-1/django/timetable/lessons/forms.py
@@ -5,20 +5,6 @@ from django import forms
 
 
 class LessonForm(ModelForm):
-    # title = forms.CharField(max_length=200)
-    # teacherName = forms.CharField(max_length=200)
-    # beginTime = forms.DateTimeField()
-    # endTime = forms.DateTimeField()
-    # link = forms.CharField(max_length=200)
-
-    # def __init__(self, *args, **kwargs):
-    #     super(Lesson, self).__init__(*args, **kwargs)
-    #     self.fields['title'].initial = kwargs.pop('title')
-    #     self.fields['teacherName'].initial = kwargs.pop('teacherName')
-    #     self.fields['beginTime'].initial = kwargs.pop('beginTime')
-    #     self.fields['endTime'].initial = kwargs.pop('endTime')
-    #     self.fields['link'].initial = kwargs.pop('link')
-    #     self.fielsds['groups'].initial = kwargs.pop('groups')
     class Meta:
         model = Lesson
         fields = ['title', 'teacherName', 'beginTime', 'endTime', 'link']
